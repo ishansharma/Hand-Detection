756933fe2725a5a79fa3b58cfa622c349dfec5c7
Ishan <contact@ishansharma.com>
2018-04-15 11:17:30 -0500

    Displaying camera, constraint madness over

--- Find Hands/ViewController.swift
+++ Find Hands/ViewController.swift
@@ -15,10 +15,56 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     @IBOutlet weak var classificationText: UILabel!
     @IBOutlet weak var cameraView: UIView!
     
+    // the video capture session obejct - this receives camera output from OS
+    let cameraViewSession = AVCaptureSession()
+    
+    // creating a queue to process the video frames
+    let frameQueue = DispatchQueue(label: "frameQueue")
+    
+    // preview layer to display the came input
+    var previewDisplayLayer: AVCaptureVideoPreviewLayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        guard let camera = AVCaptureDevice.default(for: .video) else {
+            classificationText.text = "No camera present"
+            fatalError("No camera present or available")
+        }
+        
+        cameraViewSession.sessionPreset = .high // may need to adjust this a bit
+        
+        // show the preview layer in UI
+        previewDisplayLayer = AVCaptureVideoPreviewLayer(session: cameraViewSession)
+        cameraView.layer.addSublayer(previewDisplayLayer)
+        
+        // creating capture input and video output
+        do {
+            let cameraInput = try AVCaptureDeviceInput(device: camera)
+            let videoOutput = AVCaptureVideoDataOutput()
+            videoOutput.setSampleBufferDelegate(self, queue: frameQueue)
+            videoOutput.alwaysDiscardsLateVideoFrames = true // discard late frames. Otherwise, we may have inconsistent frames
+            videoOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: kCVPixelFormatType_32BGRA]
+            
+            // connect the output and input
+            cameraViewSession.addInput(cameraInput)
+            cameraViewSession.addOutput(videoOutput)
+            
+            // ensure portrait mode
+            let conn = videoOutput.connection(with: .video)
+            conn?.videoOrientation = .portrait
+            
+            // initiate session
+            cameraViewSession.startRunning()
+        } catch _ {
+            fatalError("Could not capture camera input")
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        previewDisplayLayer.frame = self.cameraView.bounds
     }
 
     override func didReceiveMemoryWarning() {
