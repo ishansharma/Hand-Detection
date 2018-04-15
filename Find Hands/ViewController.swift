40623f7322f50d118294c0b97d6f59b7b16f2407
Ishan <contact@ishansharma.com>
2018-04-15 10:30:48 -0500

    Initial Commit

---
+++ Find Hands/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Find Hands
+//
+//  Created by Ishan Sharma on 4/15/18.
+//  Copyright © 2018 Ishan Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
